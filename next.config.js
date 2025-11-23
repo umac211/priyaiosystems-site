d1e461ab7a0bcbc20897a762df651d079af0ae2e
Uma Chowtoori <uma.chowtoori@gmail.com>
2025-11-23 18:32:41 +0530

    feat: Configure Next.js for subpath deployment by adding conditional basePath and creating .nojekyll.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
+    basePath: isProd ? "/priyaiosystems-site" : undefined,
 }
 
 module.exports = nextConfig
